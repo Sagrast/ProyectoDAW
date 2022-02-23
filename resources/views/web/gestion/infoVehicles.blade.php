8706e4943f2027dd9448af2359b431c914eafe4d
OscarGM <rikkimaru@gmail.com>
2022-02-23 16:03:02 +0100

    algo

--- resources/views/web/gestion/infoVehicles.blade.php
+++ resources/views/web/gestion/infoVehicles.blade.php
@@ -48,17 +48,13 @@
                     <tbody>
                        @foreach ($data as $car)
                             <tr class="hover:bg-grey-lighter">
-                                <td class="py-4 px-6 border-b border-grey-light">{{ $car->name }}</td>
-                                {{-- <td class="py-4 px-6 border-b border-grey-light">{{ $car->matricula }}</td>
-                                <td class="py-4 px-6 border-b border-grey-light">{{ $car->kilometros }}</td>
-                                <td class="py-4 px-6 border-b border-grey-light">{{ $car->itv }}</td> --}}
+                                <td class="py-4 px-6 border-b border-grey-light">{{ $car->name }}</td>                                {
                                 <td class="py-4 px-6 border-b border-grey-light">
                                     <a href="{{ route('web.vehicles.index') }}">
                                         <button
                                             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">Volver</button>
                                     </a>
                                 </td>
-
                             </tr>
                         @endforeach
                     </tbody>
