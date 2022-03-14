8215551fab8ed56091c3e680a17614b2ee78ef7e
Sagrast <rikkimaru@gmail.com>
2022-03-14 20:50:28 +0100

    cosas

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -163,10 +163,8 @@
         </div>
         @else
         <a href="{{route('login')}}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Login</a>
-{{--
-        Solo el administrador puede registrar nuevos usuarios.
         <a href="{{route('register')}}" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Registro</a>
- --}}
+
         @endauth
       </div>
     </div>
